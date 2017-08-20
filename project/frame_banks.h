71aace9d0d6c61826a779973cf6c3d3520e4f2c7
ProGM <progiemmeh@gmail.com>
2017-08-20 14:24:39 +0200

    Compile the old video

--- project/frame_banks.h
+++ project/frame_banks.h
@@ -1,19 +1,25 @@
 unsigned char use_frame_bank(unsigned long frame) {
-	switch(frame)
-	{
-		case 0: return 2;
-		case 147: return 3;
-		case 250: return 4;
-		case 337: return 5;
-		case 435: return 6;
-		case 572: return 7;
-		case 708: return 8;
-		case 810: return 9;
-		case 922: return 10;
-		case 1103: return 11;
-		case 1228: return 12;
-		case 1327: return 13;
-		case 1421: return 14;
-		default: return 1;
-	}
+    switch(frame)
+    {
+        case 114: return 3;
+        case 273: return 4;
+        case 387: return 5;
+        case 516: return 6;
+        case 596: return 7;
+        case 687: return 8;
+        case 796: return 9;
+        case 897: return 10;
+        case 995: return 11;
+        case 1123: return 12;
+        case 1212: return 13;
+        case 1320: return 14;
+        case 1436: return 15;
+        case 1554: return 16;
+        case 1646: return 17;
+        case 1758: return 18;
+        case 1890: return 19;
+        case 2016: return 20;
+        case 2114: return 21;
+        default: return 1;
+    }
 }
